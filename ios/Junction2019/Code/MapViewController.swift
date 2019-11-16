b55b327987f130203826219fcce7a631ba3e81e1
Otto Otsamo <ootsamo@gmail.com>
2019-11-16 18:31:19 +0200

    Show month-specific layers based on the slider

--- ios/Junction2019/Code/MapViewController.swift
+++ ios/Junction2019/Code/MapViewController.swift
@@ -13,6 +13,8 @@ class MapViewController: UIViewController {
 	private static let mapURL = URL(string: "mapbox://styles/palikk/ck31fqa9a19se1ctd00qnx31f")
 	private let mapView = MGLMapView(frame: .zero, styleURL: MapViewController.mapURL)
 	
+	var heatmapLayers = [MGLHeatmapStyleLayer]()
+	
 	let slider = UISlider()
 	
 	override func viewDidLoad() {
@@ -24,6 +26,7 @@ class MapViewController: UIViewController {
 		mapView.delegate = self
 		
 		slider.setThumbImage(UIImage(named: "knob"), for: .normal)
+		slider.addTarget(self, action: #selector(sliderValueChanged), for: .valueChanged)
 		
 		[mapView, slider].forEach {
 			$0.translatesAutoresizingMaskIntoConstraints = false
@@ -61,6 +64,13 @@ class MapViewController: UIViewController {
 		mapView.addAnnotation(annotation)
 		mapView.selectAnnotation(annotation, animated: true, completionHandler: nil)
 	}
+	
+	@objc private func sliderValueChanged(_ slider: UISlider) {
+		let month = Int((slider.value * 11).rounded())
+		for (index, heatmapLayer) in heatmapLayers.enumerated() {
+			heatmapLayer.heatmapOpacity = NSExpression(forConstantValue: 1 - min(1, abs(Double(month - index))))
+		}
+	}
 }
 
 extension MapViewController: MGLMapViewDelegate {
@@ -77,34 +87,38 @@ extension MapViewController: MGLMapViewDelegate {
 		layer.fillColor = NSExpression(forConstantValue: UIColor.red.withAlphaComponent(0.3))
 		style.addLayer(layer)
 		
-		
-		let heatmapLayer = MGLHeatmapStyleLayer(identifier: "heatmap", source: source)
-		heatmapLayer.sourceLayerIdentifier = "testipiste"
-		
-		let colors: [NSNumber: UIColor] = [
-			0: .clear,
-			0.5: UIColor(red: 0.73, green: 0.23, blue: 0.25, alpha: 0.8),
-			0.95: UIColor.orange.withAlphaComponent(0.8),
-			1: UIColor.yellow.withAlphaComponent(0.8)
-		]
-		let colorFormat = "mgl_interpolate:withCurveType:parameters:stops:($heatmapDensity, 'linear', nil, %@)"
-		heatmapLayer.heatmapColor = NSExpression(format: colorFormat, colors)
+		heatmapLayers.removeAll()
+		for i in 0...11 {
+			let heatmapLayer = MGLHeatmapStyleLayer(identifier: "heatmap\(i)", source: source)
+			heatmapLayer.sourceLayerIdentifier = String(i + 1)
+			heatmapLayer.heatmapOpacity = NSExpression(forConstantValue: i == 0 ? 1 : 0)
 			
-		let weights = [
-			0: 0,
-			10: 1
-		]
-		let weightFormat = "mgl_interpolate:withCurveType:parameters:stops:(visits, 'linear', nil, %@)"
-		heatmapLayer.heatmapWeight = NSExpression(format: weightFormat, weights)
-		
-		let intensities = [
-			0: 1,
-			9: 3
-		]
-		let intensityFormat = "mgl_interpolate:withCurveType:parameters:stops:($zoomLevel, 'linear', nil, %@)"
-		heatmapLayer.heatmapIntensity = NSExpression(format: intensityFormat, intensities)
-		
-		style.addLayer(heatmapLayer)
+			let colors: [NSNumber: UIColor] = [
+				0: .clear,
+				0.5: UIColor(red: 0.73, green: 0.23, blue: 0.25, alpha: 0.8),
+				0.95: UIColor.orange.withAlphaComponent(0.8),
+				1: UIColor.yellow.withAlphaComponent(0.8)
+			]
+			let colorFormat = "mgl_interpolate:withCurveType:parameters:stops:($heatmapDensity, 'linear', nil, %@)"
+			heatmapLayer.heatmapColor = NSExpression(format: colorFormat, colors)
+				
+			let weights = [
+				0: 0,
+				10: 1
+			]
+			let weightFormat = "mgl_interpolate:withCurveType:parameters:stops:(visits, 'linear', nil, %@)"
+			heatmapLayer.heatmapWeight = NSExpression(format: weightFormat, weights)
+			
+			let intensities = [
+				0: 1,
+				9: 3
+			]
+			let intensityFormat = "mgl_interpolate:withCurveType:parameters:stops:($zoomLevel, 'linear', nil, %@)"
+			heatmapLayer.heatmapIntensity = NSExpression(format: intensityFormat, intensities)
+			
+			style.addLayer(heatmapLayer)
+			heatmapLayers.append(heatmapLayer)
+		}
 	}
 	
 	func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
